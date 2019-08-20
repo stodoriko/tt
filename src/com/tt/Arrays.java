0bdfecd224f8a8e55c59fb29c88396380274e10e
Sergey Todoriko <sergeytodoriko@MacBook-Air-Sergey.local>
2019-08-20 23:47:30 +0300

    first commit

---
+++ src/com/tt/Arrays.java
@@ -0,0 +1,20 @@
+package com.tt;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[][] arrays = {{1, 2, 3},
+                          {4, 5, 6},
+                          {7, 8, 9}};
+
+        for (int i = 0; i < arrays.length; i++) {
+            for (int j = 0; j < arrays[i].length; j++ ) {
+                System.out.print(arrays[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+
+
+}
