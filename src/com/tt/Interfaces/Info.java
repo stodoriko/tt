5f4dcdc6372d94865fd80f54d9893af497468070
Sergey Todoriko <sergeytodoriko@MacBook-Air-Sergey.local>
2019-08-21 23:51:02 +0300

    second

---
+++ src/com/tt/Interfaces/Info.java
@@ -0,0 +1,7 @@
+package com.tt.Interfaces;
+
+// Создаю интерфейс, чтобы получить информацию из класса Animal и Person
+public interface Info {
+    // у методов интерфейса не может быть реализации
+    public void showInfo();
+}
---
+++ src/com/tt/Interfaces/Person.java
@@ -0,0 +1,17 @@
+package com.tt.Interfaces;
+
+public class Person implements Info{
+    public String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Name is " + this.name);
+    }
+}
