9ba8aa018085a720cf0f59118f8354c34fa815c1
Sergey Todoriko <sergeytodoriko@MacBook-Air-Sergey.local>
2019-08-29 23:42:41 +0300

    Enam

---
+++ src/com/tt/Enum/Month.java
@@ -0,0 +1,45 @@
+package com.tt.Enum;
+
+public enum Month {
+    // каждая из этих строк - это объект энама(класса) month
+    // Object -> Enum -> Season
+
+    JENUARY("Январь", -20),
+    FEBRUARY("Февраль", -25),
+    MARCH("Март", -5),
+    APRILL("Апрель", 5),
+    MAY("Май",15),
+    JUNE("Июнь",25),
+    JULY("Июль",30),
+    AUGUST("Август",25),
+    SEPTEMBER("Сентябрь",15),
+    OCTOMBER("Октябрь",10),
+    NOVEMBER("Ноябрь",0),
+    DECEMBER("Декабрь", -10);
+
+    private String translation;
+    private int temperature;
+
+    // Перевод на русский язык
+    Month(String translation, int temperature) {
+        this.translation = translation;
+        this.temperature = temperature;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public String getInfo() {
+        return "Средняя температура воздуха в " + translation + " равна " + temperature + "'C";
+    }
+
+    // Переопределяю метод toString()
+    public String toString() {
+        return "Перевод на русский язык месяца - " + translation;
+    }
+}
---
+++ src/com/tt/Enum/Test.java
@@ -0,0 +1,35 @@
+package com.tt.Enum;
+
+public class Test {
+    public static void main(String[] args) {
+        Month month = Month.APRILL;
+        System.out.println(month.getTranslation());
+        System.out.println(month);
+        System.out.println(month.getTemperature());
+        System.out.println(month.getInfo());
+
+        // Возвращает название энама в виде строки, так как он задекларирован
+        System.out.println(month.name());
+
+        // valueOf
+        Month aprill = Month.valueOf("APRILL");
+        System.out.println(aprill.getTranslation());
+
+        // Показывает порядковый номер перечисленных объектов
+        System.out.println(aprill.ordinal());
+
+
+        // Проверка на то, является ли объектом класса Month
+        System.out.println(month instanceof Month);
+        System.out.println(month.getClass());
+
+        switch (month) {
+            case APRILL:
+                System.out.println("Серёжка, скоро др.");
+                break;
+            case MAY:
+                System.out.println("Весна началась!");
+                break;
+        }
+    }
+}
