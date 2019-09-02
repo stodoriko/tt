c3a09c8e86a435b434b43a17b7b10a92d5c73af6
stodoriko <s.todorikos@gmail.com>
2019-09-02 22:39:55 +0300

    Нисходящее и восходящее преобразование

---
+++ src/com/tt/Downcasting_and_upcasting/Animal.java
@@ -0,0 +1,7 @@
+package com.tt.Downcasting_and_upcasting;
+
+public class Animal {
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+}
---
+++ src/com/tt/Downcasting_and_upcasting/Dog.java
@@ -0,0 +1,8 @@
+package com.tt.Downcasting_and_upcasting;
+
+public class Dog extends Animal{
+
+    public void bark() {
+        System.out.println("Dog is barking");
+    }
+}
