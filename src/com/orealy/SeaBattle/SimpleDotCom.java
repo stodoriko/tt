383d1954bf321bc9e5587e2dd15e547f171a5e17
stodoriko <s.todorikos@gmail.com>
2019-09-05 18:12:53 +0300

    O'reilly -- SeaBattle

---
+++ src/com/orealy/SeaBattle/SimpleDotCom.java
@@ -0,0 +1,34 @@
+package com.orealy.SeaBattle;
+
+public class SimpleDotCom {
+
+    private int[] locationCells;
+    private int numOfHits;
+    String userGuess;
+
+
+    public void setLocationCells(int[] locationCells) {
+        this.locationCells = locationCells;
+    }
+
+    public String checkYourSelf(String userGuess) {
+        int intUserGuess = Integer.parseInt(userGuess);
+        String result = "Мимо";
+
+        for (int cell : locationCells) {
+            if (cell == intUserGuess) {
+                result = "Попал!";
+                numOfHits++;
+                break;
+            }
+        }
+
+        if (numOfHits == locationCells.length) {
+            result = "Потопил!";
+        }
+        System.out.println(result);
+        return result;
+    }
+}
+
+
