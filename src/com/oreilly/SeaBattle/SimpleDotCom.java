47d8ca031d4036b5d5be807876fb09ff5ae8ce99
stodoriko <s.todorikos@gmail.com>
2019-09-10 22:01:49 +0300

    Занимался больше часа. Пока не могу решить задачку. Попробую попозже

--- src/com/oreilly/SeaBattle/SimpleDotCom.java
+++ src/com/oreilly/SeaBattle/SimpleDotCom.java
@@ -4,29 +4,39 @@ public class SimpleDotCom {
 
     private int[] locationCells;
     private int numOfHits;
-    String userGuess;
+    private String userInput;
+    private String result;
 
 
     public void setLocationCells(int[] locationCells) {
         this.locationCells = locationCells;
     }
 
-    public String checkYourSelf(String userGuess) {
-        int intUserGuess = Integer.parseInt(userGuess);
-        String result = "Мимо";
+    public String checkYourSelf(String userInput) {
 
-        for (int cell : locationCells) {
-            if (cell == intUserGuess) {
-                result = "Попал!";
-                numOfHits++;
-                break;
+//        while (userGuess != null) {
+        if (userInput == null) {
+            System.out.println("Вы ничего не указали.");
+        } else {
+//        }
+            int intUserInput = Integer.parseInt(userInput);
+            String result = "Мимо";
+
+            for (int cell : locationCells) {
+                if (cell == intUserInput) {
+                    result = "Попал!";
+                    numOfHits++;
+                    break;
+                }
             }
-        }
 
-        if (numOfHits == locationCells.length) {
-            result = "Потопил!";
+            if (numOfHits == locationCells.length) {
+                result = "Потопил!";
+            }
+            System.out.println(result);
+            this.result = result;
+            return result;
         }
-        System.out.println(result);
         return result;
     }
 }
--- src/com/oreilly/SeaBattle/SimpleDotComTestDrive.java
+++ src/com/oreilly/SeaBattle/SimpleDotComTestDrive.java
@@ -1,13 +1,37 @@
 package com.oreilly.SeaBattle;
 
 public class SimpleDotComTestDrive {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NumberFormatException {
+
+        int numOfGuess = 0; // переменная для количества ходов пользователя
+        String resultUserInput = null;
+
+        GameHelper helper = new GameHelper();
+
 
         SimpleDotCom simpleDotCom = new SimpleDotCom();
-        int[] locations = {2,3,4};
-        simpleDotCom.setLocationCells(locations);
-        String result = simpleDotCom.checkYourSelf("4");
+        // Генерируем случайное число для первой ячейки и используем его для формирования массива ячеек.
+        int randomNum = (int) (Math.random() * 5);
+        int[] lacations = {randomNum, randomNum + 1, randomNum + 2};
+        // int[] locations = {2,3,4};
+        // Передаём в массив местоположение ячеек
+        simpleDotCom.setLocationCells(lacations);
 
 
+        boolean isAlive = true; // Создаём переменную, чтобы проверять, не закончилась ли игра.
+
+        while (isAlive == true) {
+            // Получаем строку вводимую пользователем
+            while (resultUserInput == null) {
+                System.out.println(resultUserInput);
+                String userInput = helper.getUserInput("Введите число");
+                resultUserInput = simpleDotCom.checkYourSelf(userInput);
+                numOfGuess++;
+                if (resultUserInput.equals("Потопил")) {
+                    isAlive = false;
+                    System.out.println("Вам потребовалась" + numOfGuess + " попыток(ки)");
+                }
+            }
+        }
     }
 }
