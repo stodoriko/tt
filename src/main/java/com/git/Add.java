3a6f79b65232611a72dd435626798987be17499e
Ольга <s.todorikos@gmail.com>
2019-10-27 22:20:35 +0300

    git add .*java

---
+++ src/main/java/com/git/Add.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class Add {
+//    Разные способы сделать git add
+//    git add <список файлов> - git add file1 file2
+//    git add . - добавить все файлы в текущей папке
+//    git add *.java - добавить все файлы в текущей папке с расширением .java
+//    git add SomeDir/*.java - добавить все файлы из папки SomeDir с расширением .java
+//    git add SomeDir/ - добавить все файлы из папки SomeDir
+//    git add "*.java" - добавить все файлы в проекте с расширением .java
+}
