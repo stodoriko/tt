a0f36f4eae084af260a46260cd5c1f99157c0a7d
Ольга <s.todorikos@gmail.com>
2020-01-15 23:57:21 +0300

    Constructors

---
+++ src/main/java/com/tt/java_1/Constructors/Human.java
@@ -0,0 +1,34 @@
+package com.tt.java_1.Constructors;
+
+import java.util.ArrayList;
+
+class Human {
+
+    private int age;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String birthPlace;
+    private String fullInfo;
+    private ArrayList<String> humansList = new ArrayList<>();
+
+    public Human(int age, String firstName, String lastName, String gender, String birthPlace) {
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.birthPlace = birthPlace;
+
+        String fullInfo = firstName + " " + lastName + "; " + age + "; " + gender + "; " + birthPlace;
+
+        for (int i = 0; i == humansList.size(); i++) {
+            humansList.add(fullInfo);
+        }
+
+        System.out.println(humansList.toString());
+    }
+
+    public String getFullInfo() {
+        return fullInfo;
+    }
+}
