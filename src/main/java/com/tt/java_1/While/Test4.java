36b1ff24dabc4143d32ef4476db19abf722045dd
stodoriko <s.todorikos@gmail.com>
2020-03-31 14:59:58 +0300

    JR while 4.10_2

---
+++ src/main/java/com/tt/java_1/While/Test4.java
@@ -0,0 +1,22 @@
+package com.tt.java_1.While;
+
+public class Test4 {
+    public static void main(String[] args) {
+
+//        Вывести на экран таблицу умножения 10х10 используя цикл while.
+//        Числа разделить пробелом.
+
+        int a = 1;
+
+        while (a < 11) {
+            int b = 1;
+            while (b < 11) {
+                int c = a * b;
+                System.out.print(c + " ");
+                b++;
+            }
+            System.out.println();
+            a++;
+        }
+    }
+}
