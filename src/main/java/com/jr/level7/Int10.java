58d51ad279969228a02f8e0c673c57d5db5eb040
stodoriko <s.todorikos@gmail.com>
2020-05-28 19:39:36 +0300

    jr level7 -- Int[10] and revert

---
+++ src/main/java/com/jr/level7/Int10.java
@@ -0,0 +1,35 @@
+package com.jr.level7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Int10 {
+    public static void main(String[] args) throws IOException {
+        // Создаём целочисленный массив с длинной указанной в параметре
+        int[] array = initIntArray(10);
+        // Вызываем статический метод для заполнения массива значениями введёнными с клавиатуры
+        int[] arrayAfterFilling = arrayFilling(array);
+        // Вызываем статический метод, который выводит на экран значения массива в обратной последовательности
+        revertArray(arrayAfterFilling);
+    }
+
+    public static int[] initIntArray(int a) {
+        return new int[a];
+    }
+
+    public static int[] arrayFilling(int[] array) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(bf.readLine());
+        }
+        return array;
+    }
+
+    public static void revertArray(int[] arrayAfterFilling) {
+        for(int i = arrayAfterFilling.length; i > 0; i--) {
+            System.out.println(arrayAfterFilling[i - 1]);
+        }
+    }
+}
