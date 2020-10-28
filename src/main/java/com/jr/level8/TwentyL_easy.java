04b7e421537d2c0c6a6a91e11ecee63411ed797b
sergeytodoriko <s.todorikos@gmail.com>
2020-10-28 22:39:42 +0300

    level8 TwentyL_easy

---
+++ src/main/java/com/jr/level8/TwentyL_easy.java
@@ -0,0 +1,27 @@
+package com.jr.level8;
+
+/*
+Требования:
+•	Не изменяй заголовок метода createSet().
+•	Программа не должна выводить текст на экран.
+•	Программа не должна считывать значения с клавиатуры.
+•	Метод createSet() должен создавать и возвращать множество (реализация HashSet).
+•	Множество из метода createSet() должно содержать 20 слов на букву «Л».
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TwentyL_easy {
+    public static Set<String> createSet() {
+        Set<String> set = new HashSet<String>();
+        for (int i = 0; i < 20; i++) {
+            set.add("Л" + i);
+        }
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
