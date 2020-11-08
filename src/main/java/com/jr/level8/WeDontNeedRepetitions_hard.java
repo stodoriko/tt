14364dc10521586efd6661c86ffcb532f0ea0ea0
sergeytodoriko <s.todorikos@gmail.com>
2020-11-08 21:25:10 +0300

    level8 WeDontNeedRepetitions_hard

--- src/main/java/com/jr/level8/WeDontNeedRepetitions_hard.java
+++ src/main/java/com/jr/level8/WeDontNeedRepetitions_hard.java
@@ -31,17 +31,61 @@ public class WeDontNeedRepetitions_hard {
         ArrayList<String> listOfValues = new ArrayList<>();
         ArrayList<String> listOfEquals = new ArrayList<>();
 
-        for (Map.Entry<String, String> it : map.entrySet()) {
-            listOfValues.add(it.getValue());
+        Iterator<Map.Entry<String, String>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, String> pair = it.next();
+            listOfValues.add(pair.getValue());
         }
-        for (int i = 0; i < listOfValues.size(); i++) {
-            if (i > 0) {
-                if (listOfValues.get(i).equals(listOfValues.get(i - 1))) {
+        for (int k = 0; k < listOfValues.size(); k++) {
+            System.out.println(listOfValues.get(k));
+        }
+        System.out.println("========================");
+        int s = 0;
+
+        for (int i = s; i < listOfValues.size(); i++) {
+            System.out.println("итерация: " + i + " Сравнение с именем: " + listOfValues.get(i));
+            for (int l = i+1; l < listOfValues.size(); l++) {
+                System.out.println("Сравниваем " + listOfValues.get(i) + " и " + listOfValues.get(l));
+                if (listOfValues.get(i).equals(listOfValues.get(l))) {
                     listOfEquals.add(listOfValues.get(i));
+                    System.out.println("Удалён элемент с именем " + listOfValues.get(l));
+                    listOfValues.remove(l);
                 }
             }
-            System.out.println(listOfEquals.size());
+            System.out.println("Удалён элемент с именем " + listOfValues.get(i));
+            listOfValues.remove(i);
+            i--;
         }
+
+        System.out.println("=======================");
+
+        for (int j = 0; j < listOfEquals.size(); j++) {
+            System.out.println(listOfEquals.get(j));
+        }
+        System.out.println(map.size());
+        System.out.println("=======================");
+
+
+        Iterator<Map.Entry<String, String>> it2 = map.entrySet().iterator();
+        while (it2.hasNext()) {
+            Map.Entry<String, String> pair = it2.next();
+            for (int h = 0; h < listOfEquals.size(); h++) {
+                if (pair.getValue().equals(listOfEquals.get(h))) {
+                    it2.remove();
+                    System.out.println(map.size());
+                }
+            }
+        }
+
+        Iterator<Map.Entry<String, String>> it3 = map.entrySet().iterator();
+        while (it3.hasNext()) {
+            Map.Entry<String, String> pair = it3.next();
+            System.out.println(pair.getKey() + " " + pair.getValue());
+        }
+
+
+
+        removeItemFromMapByValue(createMap(), listOfEquals.get(0));
     }
 
     public static void removeItemFromMapByValue(Map<String, String> map, String value) {
