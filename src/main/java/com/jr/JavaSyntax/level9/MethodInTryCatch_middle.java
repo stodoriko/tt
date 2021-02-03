130e8d8e5be0bc21f8fbd66833b852ace91b916f
sergeytodoriko <s.todorikos@gmail.com>
2021-02-04 01:07:55 +0300

    level9 -- MethodInTryCatch_middle_middle

---
+++ src/main/java/com/jr/JavaSyntax/level9/MethodInTryCatch_middle.java
@@ -0,0 +1,37 @@
+package com.jr.JavaSyntax.level9;
+
+//        Написать программу, которая будет вводить числа с клавиатуры.
+//        Код по чтению чисел с клавиатуры должен быть в методе readData.
+//        Код внутри readData обернуть в try..catch.
+//        Если пользователь ввёл какой-то текст, вместо ввода числа, то метод должен перехватить исключение и вывести на экран все ранее введенные числа в качестве результата.
+//        Числа выводить с новой строки сохраняя порядок ввода.
+
+import cucumber.api.java.sl.In;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class MethodInTryCatch_middle {
+    public static void main(String[] args){
+        readData();
+    }
+
+    public static void readData() {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (true) {
+            try {
+                int i = Integer.parseInt(bf.readLine());
+                list.add(i);
+            } catch (IOException | NumberFormatException e) {
+                for (int i : list) {
+                    System.out.println(i);
+                }
+                break;
+            }
+        }
+    }
+}
