5961d4a0c237d43166f0346b2b7e957ba853545c
sergeytodoriko <s.todorikos@gmail.com>
2021-08-12 10:41:07 +0300

    level10 -- 11

---
+++ src/main/java/com/jr/JavaSyntax/level10/Eleven_easy.java
@@ -0,0 +1,11 @@
+package com.jr.JavaSyntax.level10;
+
+public class Eleven_easy {
+    public static void main(String[] args) {
+        String text = "Я не хочу изучать Java, я хочу большую зарплату";
+        int str = text.length();
+        System.out.println(str);
+
+        
+    }
+}
